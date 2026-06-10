a57b4b6bc4a919c6dfeb37ffdf0d944c0aa76560
ydhsjy <al.yudha.san@gmail.com>
2026-06-10 19:41:20 +0700

    Restore section divider background

--- src/sections/SectionDivider.tsx
+++ src/sections/SectionDivider.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import { wedding } from "@/data/wedding";
 
 type SectionDividerProps = {
   image?: string;
   position?: "top" | "bottom";
 };
 
-export function SectionDivider({ image, position = "top" }: SectionDividerProps) {
+export function SectionDivider({ image = wedding.images.sectionDivider, position = "top" }: SectionDividerProps) {
   return (
     <section className="relative h-32 overflow-hidden bg-white sm:h-40 lg:h-28" aria-hidden="true">
       {image ? (
